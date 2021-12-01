e1bc22d6f8db57149b87f19639e25304b83a0ee9
Vazn <cavazzoni.chris@gmail.com>
2021-12-01 23:35:25 +0100

    Addind Doubly LinkedList, updating LinkedList

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -1,40 +1,77 @@
 const Node = require('./node.js');
 
-function LinkedList() {
+function DoublyLinkedList() {
 
     this.head = null;
+    this.tail = null;
 
     this.addToHead = function(data) {
         const newHead = new Node(data);
         const currentHead = this.head;
+
+        if (currentHead) 
+        {
+            currentHead.setPreviousNode(newHead);
+            newHead.setNextNode(currentHead);
+        }
         this.head = newHead;
 
-        newHead.setNextNode(currentHead);                  
+        if (!this.tail) 
+        {
+            this.tail = this.head;
+        }
     };
 
     this.addToTail = function(data) {
-        let tail = this.head;
+        const newTail = new Node(data);
+        const currentTail = this.tail;
 
-        if (tail === null) {
-            this.head = new Node(data);
+        if (currentTail) {
+            currentTail.setNextNode(newTail);
+            newTail.setPrevNode(currentTail);
         }
-        else {          
-            while (tail.getNextNode() !== null)             
-            {
-              tail = tail.getNextNode();
-            } 
-            tail.setNextNode(new Node(data));
+        this.tail = newTail;
+       
+        if (!this.head) 
+        {
+            this.head = this.tail;
         }
     };
 
     this.removeHead = function() {
         const removedHead = this.head;
 
-        if (removedHead === null) {
-            return;
+        if (!removedHead) {
+          return;
         }
-        this.head = removedHead.getNextNode()
+
+        this.head = removedHead.getNextNode();
+
+        if (this.head) {
+          this.head.setPrevNode(null);
+        }
+        if (removedHead === this.tail) {
+          this.removeTail();
+        }
         return removedHead.data;
+    };
+
+    this.removeTail = function() {
+        const removedTail = this.tail;
+
+        if (!removedTail) {
+          return;
+        }
+
+        this.tail = removedTail.getPrevNode();
+
+        if (this.tail) {
+          this.head.setNextNode(null);
+        }
+        if (removedTail === this.head) {
+          this.removeHead();
+        }
+        return removedTail.data;
     };
 
     this.removeByData = function() {
@@ -54,9 +91,13 @@ function LinkedList() {
         if (nodeToRemove == this.head) {
             this.removeHead();
         }
+        else if (nodeToRemove == this.tail) {
+            this.removeTail();
+        }
         else {
             const nextNode = nodeToRemove.getNextNode();
             const prevNode = nodeToRemove.getPrevNode();
+            nextNode.setPrevNode(prevNode);
             prevNode.setNextNode(nextNode);
         }
         return nodeToRemove;
@@ -77,4 +118,4 @@ function LinkedList() {
     };
 };
 
-module.exports = LinkedList;
+module.exports = DoublyLinkedList;
